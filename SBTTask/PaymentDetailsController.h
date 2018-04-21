a530fc057be9bc4ca20c20217531d8fe304641df
IsaAliev <isaaliev12@gmail.com>
2018-04-21 18:11:02 +0300

    Initial commit

--- SBTTask/PaymentDetailsController.h
+++ SBTTask/PaymentDetailsController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  PaymentDetailsController.h
 //  SBTTask
 //
 //  Created by Isa Aliev on 21.04.2018.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface PaymentDetailsController : UITableViewController
 
 @end
-
