8c217a599ed2ab805788cffb6e050f6ef4be8462
aliev <aliev@telemed.help>
2018-04-23 13:52:02 +0300

    Add returning from text view

--- SBTTask/Payment Details/Cells/PaymentDetailsNameSelectionCell.m
+++ SBTTask/Payment Details/Cells/PaymentDetailsNameSelectionCell.m
@@ -57,4 +57,14 @@
     ((PaymentDetailsNameSelectionCellModel *)[self viewModel]).name = textView.text;
 }
 
+-(BOOL)textView:(UITextView *)textView shouldChangeTextInRange:(NSRange)range replacementText:(NSString *)text {
+    if ([text isEqualToString:@"\n"]) {
+        [textView resignFirstResponder];
+        
+        return NO;
+    }
+    
+    return YES;
+}
+
 @end
